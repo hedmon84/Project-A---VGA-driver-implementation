dd5a7a93e056b4e3b877fbee175acf30a0768b70
hedmon84 <hedmonlopes@gmail.com>
2020-05-04 21:22:02 -0600

    works function screen

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 int main()
 {
     // clear_screen(BLUE);
-    // puts(10, 10, MAGENTA, BLACK, "Hello");
-    // puts(10, 16, YELLOW, BLACK, "World");
+    puts(10, 10, MAGENTA, BLACK, "Hello");
+    puts(10, 16, YELLOW, BLACK, "World");
     // return 0;
 
     // Example //
@@ -13,11 +13,13 @@ int main()
     // end address  0xcabe
     //every  column has a length of 80
     //every row has a size of 30
-    put(10, 5, BLUE, GREEN);
+    // puts(0, 5, YELLOW, WHITE, "hola");
 
     // uint16_t *p = (uint16_t *)0xb800;
     // *(p + 320) = 0x2f00 | 'h';
     //*(p + 1) = 0xcf00 | 'o';
     // *(p + 2) = 0xcf00 | 'l';
     // *(p + 3) = 0xcf00 | 'a';
+
+    return 0;
 }
--- src/screen.c
+++ src/screen.c
@@ -4,20 +4,29 @@
 // {
 // }
 
-void put(uint8_t row, uint8_t col, uint16_t fgcolor, uint16_t bgcolor)
+void puts(uint8_t row, uint8_t col, uint16_t fgcolor, uint16_t bgcolor, char *message) // works perfect
 {
 
-    uint16_t a = 0x1; //blue
-    uint16_t b = 0x2; //green
+    int i = 0;
 
     uint16_t rw = 80 * row + col;
-    // no agarra cuando le mando fgcolor , bgcolor
 
     // a corre tres bits  y una a b y saca el valor que refleja el color de fondo y letra
     uint16_t color = (bgcolor << 4) | (fgcolor);
 
+    // *(p + rw) = (color << 8) | *message;
+
+    // *(p + rw + 1) = (color << 8) | message[1]; // message[] accessing to the address every char is located
+    // *(p + rw + 2) = (color << 8) | message[2]; // message[] accessing to the address every char is located
+
     uint16_t *p = (uint16_t *)0xb800;
-    *(p + rw) = (color << 8) | 'h';
+
+    while (message[i] != 0)
+    {
+
+        *(p + rw + i) = (color << 8) | message[i];
+        i++;
+    }
 }
 
 void clear_screen(uint8_t bgcolor)
